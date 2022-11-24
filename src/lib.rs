885a95ef48103b9f6ebb626eeec7588201af94a0
SnowballSH <derek070211@gmail.com>
2022-11-23 20:23:47 -0600

    no WEE

--- src/lib.rs
+++ src/lib.rs
@@ -3,10 +3,6 @@ mod utils;
 use wasm_bindgen::prelude::*;
 use crate::utils::set_panic_hook;
 
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
 #[wasm_bindgen]
 extern {
     fn alert(s: &str);
