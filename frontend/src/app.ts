6200f3bba2b405a73dcc0209d9201b52fca4ee02
SnowballSH <derek070211@gmail.com>
2022-11-26 22:14:24 -0600

    more info

--- frontend/src/app.ts
+++ frontend/src/app.ts
@@ -1,5 +1,5 @@
 import {css, html, LitElement} from 'lit';
-import {customElement} from 'lit/decorators.js';
+import {customElement, property} from 'lit/decorators.js';
 import 'wired-elements';
 
 import './plotInput';
@@ -11,10 +11,19 @@ import * as backend from '../pkg/';
 export class App extends LitElement {
     model: backend.DynamicNetwork | null = null;
 
+    @property({type: String})
+    cost: number = NaN;
+
+    @property({type: String})
+    trainingProgress: string = "N/A";
+
+    @property({type: String})
+    numCorrect: string = "N/A";
+
     connectedCallback() {
         super.connectedCallback();
         backend.init();
-        this.model = backend.DynamicNetwork.new_from_js(new Uint32Array([2, 4, 8, 3]));
+        this.model = backend.DynamicNetwork.new_from_js(new Uint32Array([2, 8, 4, 3]));
         this.model.randomize_from_js();
     }
 
@@ -27,6 +36,11 @@ export class App extends LitElement {
                     <wired-button @click=${this.learn}>Learn</wired-button>
                     <wired-button @click=${this.randomize}>Randomize</wired-button>
                 </p>
+                <p>
+                <h3>Current Cost: ${this.cost}</h3>
+                <h3>Training Progress: ${this.trainingProgress}</h3>
+                <h3>Number Correct: ${this.numCorrect}</h3>
+                </p>
                 <plot-input id="plti"></plot-input>
                 </body>
             </div>
@@ -35,6 +49,8 @@ export class App extends LitElement {
 
     randomize() {
         this.model!.randomize_from_js();
+        this.cost = NaN;
+        this.trainingProgress = "N/A";
     }
 
     evaluate() {
@@ -43,7 +59,7 @@ export class App extends LitElement {
         let realCtx = plti.realCanvas!.getContext("2d")!;
 
         const colors = ['#ff9999', '#9eb9ff', '#a9ffa8'];
-        const delta = 4;
+        const delta = 3;
 
         realCtx.clearRect(0, 0, plti.width, plti.height);
 
@@ -62,14 +78,22 @@ export class App extends LitElement {
         let dataY = plti.outputs;
 
         let cost = 0;
+        let correct = 0;
 
         for (let i = 0; i < dataX.length; i++) {
             const xs = dataX[i];
             const ys = dataY[i];
             cost += this.model!.mse_from_js(new Float32Array(xs), new Float32Array(ys));
+
+            let output = this.model!.forward_from_js(new Float32Array(xs));
+            let k = output.reduce((iMax, x, i, arr) => x > arr[iMax] ? i : iMax, 0);
+            if (k === ys.indexOf(1)) {
+                correct++;
+            }
         }
 
-        console.log(cost);
+        this.cost = cost;
+        this.numCorrect = `${correct}/${dataX.length}`;
     }
 
     async learn() {
@@ -91,9 +115,10 @@ export class App extends LitElement {
 
         for (let j = 0; j < 100; j++) {
             for (let i = 0; i < 200; i++) {
-                this.model!.learn_from_js(input_builder, output_builder, 0.015 - j * 0.0001);
+                this.model!.learn_from_js(input_builder, output_builder, 0.035 - j * 0.0001);
             }
             this.evaluate();
+            this.trainingProgress = `${j + 1}/100`;
             await new Promise(r => setTimeout(r, 20));
         }
 
